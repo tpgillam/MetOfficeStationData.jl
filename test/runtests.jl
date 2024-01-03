5d96f8399c072ddfdb508ab061680d1879dab508
Tom Gillam <tpgillam@googlemail.com>
2024-01-03 22:18:27 +0000

    Format

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,9 @@ using Test
 Mocking.activate()
 
 function _mock_download(short_name::AbstractString)
-    return String(open(read, joinpath(dirname(@__FILE__), "reference", "$(short_name)data.txt")))
+    return String(
+        open(read, joinpath(dirname(@__FILE__), "reference", "$(short_name)data.txt"))
+    )
 end
 
 @testset "MetOfficeStationData.jl" begin
@@ -21,7 +23,7 @@ end
             df = MetOfficeStationData.get_frame(short_name)
             @test isa(df, DataFrame)
             @test ncol(df) == 7
-            @test names(df) ==  ["yyyy", "mm", "tmax", "tmin", "af", "rain", "sun"]
+            @test names(df) == ["yyyy", "mm", "tmax", "tmin", "af", "rain", "sun"]
         end
     end
 end
